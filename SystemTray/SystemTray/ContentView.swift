8097b7eab4d4b028a17993910cab820d4ad5794d
Jesús Antonio Gil <jesus.antoniogil@telefonica.com>
2025-03-08 17:20:45 +0100

    Create project

---
+++ SystemTray/SystemTray/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  SystemTray
+//
+//  Created by Jesus Antonio Gil on 8/3/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ SystemTray/SystemTray/SystemTrayApp.swift
@@ -0,0 +1,17 @@
+//
+//  SystemTrayApp.swift
+//  SystemTray
+//
+//  Created by Jesus Antonio Gil on 8/3/25.
+//
+
+import SwiftUI
+
+@main
+struct SystemTrayApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
