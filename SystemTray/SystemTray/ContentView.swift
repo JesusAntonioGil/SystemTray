c91fd2b6fe93c8bb601f782d69d91ab577e2cf43
Jesús Antonio Gil <jesus.antoniogil@telefonica.com>
2025-03-09 23:58:05 +0100

    System Tray project done

--- SystemTray/SystemTray/ContentView.swift
+++ SystemTray/SystemTray/ContentView.swift
@@ -11,12 +11,16 @@ import SwiftUI
 enum CurrentView {
     case actions
     case periods
+    case keypad
 }
 
 
 struct ContentView: View {
     @State private var show: Bool = false
     @State private var currentView: CurrentView = .actions
+    @State private var selectedAction: Action?
+    @State private var selectedPeriod: Period?
+    @State private var duration: String = ""
     
     
     var body: some View {
@@ -24,9 +28,37 @@ struct ContentView: View {
             show.toggle()
         }
         .systemTrayView($show) {
-            Text("Hello From Tray")
-                .frame(maxWidth: .infinity)
-                .frame(height: 300)
+            VStack(spacing: 20) {
+                ZStack {
+                    switch currentView {
+                        case .actions:
+                            View1()
+                                .transition(.blurReplace)
+                        case .periods:
+                            View2()
+                                .transition(.blurReplace)
+                        case .keypad:
+                            View3()
+                                .transition(.blurReplace)
+                    }
+                }
+                .compositingGroup()
+                
+                Button {
+                    withAnimation(.bouncy) {
+                        currentView = .periods
+                    }
+                } label: {
+                    Text("Continue")
+                        .fontWeight(.semibold)
+                        .frame(maxWidth: .infinity)
+                        .padding(.vertical, 15)
+                        .foregroundStyle(.white)
+                        .background(.blue, in: .capsule)
+                }
+                .padding(.top, 15)
+            }
+            .padding(20)
         }
     }
     
@@ -36,7 +68,176 @@ struct ContentView: View {
         VStack(spacing: 12) {
             HStack {
                 Text("Choose description")
+                    .font(.title2)
+                    .fontWeight(.semibold)
+                
+                Spacer(minLength: 0)
+                
+                Button {
+                    show = false
+                } label: {
+                    Image(systemName: "xmark.circle.fill")
+                        .font(.title)
+                        .foregroundStyle(Color.gray, Color.primary.opacity(0.1))
+                }
             }
+            .padding(.bottom, 10)
+            
+            ForEach(actions) { action in
+                let isSelected: Bool = selectedAction?.id == action.id
+                
+                HStack(spacing: 10) {
+                    Image(systemName: action.image)
+                        .font(.title)
+                        .frame(width: 40)
+                    
+                    Text(action.title)
+                        .fontWeight(.semibold)
+                    
+                    Spacer(minLength: 0)
+                    
+                    Image(systemName: isSelected ? "checkmark.circle.fill" : "circle.fill")
+                        .font(.title)
+                        .contentTransition(.symbolEffect)
+                        .foregroundStyle(isSelected ? Color.blue : Color.gray.opacity(0.2))
+                }
+                .padding(.vertical, 6)
+                .contentShape(.rect)
+                .onTapGesture {
+                    withAnimation(.snappy) {
+                        selectedAction = isSelected ? nil : action
+                    }
+                }
+            }
+        }
+    }
+    
+    
+    @ViewBuilder
+    private func View2() -> some View {
+        VStack(spacing: 12) {
+            HStack {
+                Text("Choose period")
+                    .font(.title2)
+                    .fontWeight(.semibold)
+                
+                Spacer(minLength: 0)
+                
+                Button {
+                    withAnimation(.bouncy) {
+                        currentView = .actions
+                    }
+                } label: {
+                    Image(systemName: "xmark.circle.fill")
+                        .font(.title)
+                        .foregroundStyle(Color.gray, Color.primary.opacity(0.1))
+                }
+            }
+            .padding(.bottom, 25)
+            
+            Text("Choose the period you want\nto get subscribed.")
+                .multilineTextAlignment(.center)
+                .foregroundStyle(.gray)
+                .padding(.bottom, 20)
+            
+            LazyVGrid(columns: Array(repeating: GridItem(), count: 3), spacing: 15) {
+                ForEach(periods) { period in
+                    let isSelected = selectedPeriod?.id == period.id
+                    
+                    VStack(spacing: 6) {
+                        Text(period.title)
+                            .font(period.value == 0 ? .title3 : .title2)
+                            .fontWeight(.semibold)
+                        
+                        if period.value != 0 {
+                            Text(period.value == 1 ? "Month" : "Months")
+                                .font(.caption)
+                                .foregroundStyle(.gray)
+                        }
+                    }
+                    .frame(maxWidth: .infinity)
+                    .frame(height: 80)
+                    .background {
+                        RoundedRectangle(cornerRadius: 20)
+                            .fill(isSelected ? Color.blue : Color.gray).opacity(isSelected ? 0.2 : 0.1)
+                    }
+                    .contentShape(.rect)
+                    .onTapGesture {
+                        withAnimation(.snappy) {
+                            if period.value == 0 {
+                                currentView = .keypad
+                            } else {
+                                selectedPeriod = isSelected ? nil : period
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
+    
+    
+    @ViewBuilder
+    private func View3() -> some View {
+        VStack(spacing: 12) {
+            HStack {
+                Text("Custom duration")
+                    .font(.title2)
+                    .fontWeight(.semibold)
+                
+                Spacer(minLength: 0)
+                
+                Button {
+                    withAnimation(.bouncy) {
+                        currentView = .periods
+                    }
+                } label: {
+                    Image(systemName: "xmark.circle.fill")
+                        .font(.title)
+                        .foregroundStyle(Color.gray, Color.primary.opacity(0.1))
+                }
+            }
+            .padding(.bottom, 10)
+            
+            VStack(spacing: 10) {
+                Text(duration.isEmpty ? "0" : duration)
+                    .font(.system(size: 40, weight: .black))
+                    .contentTransition(.numericText())
+                
+                Text("Days")
+                    .font(.caption)
+                    .foregroundStyle(.gray)
+            }
+            .padding(.vertical, 20)
+            
+            LazyVGrid(columns: Array(repeating: GridItem(), count: 3), spacing: 15) {
+                ForEach(keypadValues) { keyValue in
+                    Group {
+                        if keyValue.isBack {
+                            Image(systemName: keyValue.title)
+                        } else {
+                            Text(keyValue.title)
+                        }
+                    }
+                    .font(.title2)
+                    .fontWeight(.semibold)
+                    .frame(maxWidth: .infinity)
+                    .frame(height: 60)
+                    .contentShape(.rect)
+                    .onTapGesture {
+                        withAnimation(.snappy) {
+                            if keyValue.isBack {
+                                if !duration.isEmpty {
+                                    duration.removeLast()
+                                }
+                            } else {
+                                duration.append(keyValue.title)
+                            }
+                        }
+                    }
+                }
+            }
+            .padding(.horizontal, -15)
         }
     }
 }
